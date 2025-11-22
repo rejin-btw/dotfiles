34bf55219295d134e93e0ab471bdffe7cc6c4fa2
rejin-btw <rejinks@zohomail.in>
2025-11-22 09:42:23 +0530

    change

--- datetime_notify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script to display current date and time as notification
-
-# Get current date and time
-current_datetime=$(date "+%A, %B %d, %Y%n%I:%M:%S %p")
-
-# Send notification
-notify-send "Current Date & Time" "$current_datetime" --icon=clock
-
