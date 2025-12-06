a6718a4adbd8e316e8c0acf5844e70cb7bd73521
rejin-btw <rejinks@zohomail.in>
2025-12-06 23:55:34 +0530

    added the config for pistol, changed lf config, added the auto-image preview functionality

--- .config/fish/conf.d/rejin.fish
+++ .config/fish/conf.d/rejin.fish
@@ -5,6 +5,9 @@ alias hm="nvim ~/nixos-flake/home/rejin.nix"
 alias sm="nvim ~/nixos-flake/hosts/default.nix"
 alias fishc="nvim ~/dotfiles/.config/fish/conf.d/rejin.fish"
 alias starshipc="nvim ~/dotfiles/.config/starship.toml"
+alias hmr="home-manager switch --flake ~/nixos-flake/.#rejin"
+alias smr="sudo nixos-rebuild switch --flake ~/nixos-flake/.#rejin-nixos"
+alias nfu="nix flake update --flake ~/nixos-flake"
 
 # My Settings
 set -g fish_greeting ""
@@ -20,6 +23,7 @@ set fish_cursor_insert line
 # Replace Mode = Underscore _
 set fish_cursor_replace_one underscore
 set fish_cursor_visual block
+set -gx EDITOR nvim
 
 # 2. Disable the [I] / [N] text indicator
 # (We don't need it because the cursor shape tells us the mode now)
@@ -34,6 +38,26 @@ end
     #end
 
 # ... existing aliases ...
+
+
+# Function: Add -> Commit -> Push in one go
+function gcp
+    # 1. Check if message was provided
+    if test (count $argv) -eq 0
+        echo "Error: Missing commit message."
+        echo "Usage: gcp \"Updated niri config and fixed shell aliases\""
+        return 1
+    end
+
+    # 2. Add all changes (both new and modified files)
+    git add .
+
+    # 3. Commit with the message provided in arguments
+    git commit -m "$argv"
+
+    # 4. Push to the current branch
+    git push
+end
 
 
 
